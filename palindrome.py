9ddcf0ff13472d3e6e8c469a57ecd4ad3775ef07
Shwetha.Baligeri <sujatasujata45539@gmail.com>
2025-11-14 11:01:14 +0530

    palindrome modified

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,24 @@
-string = input("Enter a string: ")
+import sys
 
-# Reverse the string
-rev = string[::-1]
+print("=== Palindrome Checker ===")
 
-if string == rev:
-    print("The string is a palindrome")
+# Take input from CLI or user
+if len(sys.argv) > 1:
+    text = " ".join(sys.argv[1:])
 else:
-    print("The string is not a palindrome")
+    text = input("Enter a string: ")
+
+# Count alphabetic letters only
+letters = "".join(ch.lower() for ch in text if ch.isalpha())
+
+# Check minimum length
+if len(letters) < 4:
+    print("Error: Please enter at least 4 letters.")
+    print("Letters found:", len(letters))
+    exit()
+
+# Palindrome check
+if letters == letters[::-1]:
+    print("Yes, it is a palindrome.")
+else:
+    print("No, it is not a palindrome.")
